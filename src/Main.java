d63e946ab35f28c145dbc8ef36b527d587f2ad0b
Valentin Oprea <aeroprea@gmail.com>
2022-06-06 13:45:03 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Pong game
+		
+		new GameFrame();
+	}
+
+}
